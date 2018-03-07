76267d8101886a9e614b87396d9975753cde8780
Shahnur Isgandarli <sisgandarli2018@ada.edu.az>
2018-03-07 23:45:55 +0400

    Added s, y, n suffixes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     # Instantiate Stemmer object.
     my_stemmer = Stemmer()
     # Generate your text.
-    my_text = "Təbii dilin emalı"
+    my_text = "Təbii dilin emalı. Dənizin suyu. Qızın çöhrəsi. Onun kitabının."
     # Preprocess your text: remove punctuation, lowercase the letters, trim the spaces and newlines, and split the text by space/s.
     my_words = my_text.translate(my_text.maketrans(string.punctuation, " " * len(string.punctuation))).lower().strip().split()
     # Print the generated array of words (not stemmed yet).
