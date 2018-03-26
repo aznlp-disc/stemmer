94111ecd5aa71b28af45c82fa0e8dd5a4247bad4
Ali Mustafali <mustafa.ali0096@gmail.com>
2018-03-26 15:48:49 +0400

    Update tagger.py

--- tagger.py
+++ tagger.py
@@ -4,20 +4,25 @@ from stemmer import Stemmer
 
 class Tagger:
     dictionary = {}
+    punctuation = [',', '.', ':', ';']
+    text = ''
     def __init__(self, file):
         self.load_dict()
         nstem = self.tag(file)
-        # print(nstem)
         stem = Stemmer().stem_words(nstem)
-        # print(stem)
         tags = self.list_tags(stem)
         print(tags)
+        # list of original words in the text
+        list_of_words = self.text.split()
+        print(list_of_words)
+
+        self.save_to('cavab.txt', list_of_words, ["a", "b", "c", "e", "f", "g", "h", "i", "j"])
 
     def tag(self, file):
         with open(file, "r", encoding="utf8") as my_text:
             my_text = my_text.read()
-
-        print(my_text)
+        self.text = my_text
+        print(self.text)
 
         my_text = my_text.replace("İ", "I")
         my_text = my_text.replace("“", "")
@@ -45,9 +50,25 @@ class Tagger:
             try:
                 tags.append((st, str(self.dictionary[self.upp((str(st))).upper()]).split(';')))
             except Exception:
-                tags.append((st, []))
+                tags.append((st, ['']))
 
         return tags
+
+    def is_punctuation(self, symbol):
+        for punct in self.punctuation:
+            if symbol == punct:
+                return True
+        return False
+
+    def save_to(self, filename, text, tags):
+        file = open(filename, "w", encoding = 'utf8')
+        for i in range(0, len(text)):
+            if self.is_punctuation(text[i][-1]):
+                file.write(text[i][:-1]+'{'+tags[i]+'}'+text[i][-1]+' ')
+            else:
+                file.write(text[i] + '{' + tags[i] + '} ')
+
+        file.close()
 
 
 tagi = Tagger('yoxla.txt')
