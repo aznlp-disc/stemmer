1532a350cee23fd14878c564c0fa4af8737f6625
Samir Mammadov <m.samir1996@gmail.com>
2018-04-10 17:05:07 +0400

    Fixes #23

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-# Import the Stemmer class from stemmer.py file.
 from stemmer import Stemmer
-# Import string.
-import string
+from string import punctuation
 
 # Program starts here.
 if __name__ == '__main__':
-    # Instantiate Stemmer object.
+    # Instantiate Stemmer object
     my_stemmer = Stemmer()
-    # Generate your text.
-    with open("text.txt", 'r', encoding="utf-8-sig") as text:
+    # Generate your text
+    with open("quba.txt", 'r', encoding="utf-8-sig") as text:
         my_text = text.read()
-    # Preprocess your text: remove punctuation, lowercase the letters, trim the spaces and newlines, and split the text by space/s.
+    # Preprocess your text: remove punctuation, lowercase the letters, trim the spaces and newlines, and split the text by space/s
     my_text=my_text.replace("İ", "I")
     my_text=my_text.replace("“", "")
     my_text=my_text.replace("”", "")
     my_text=my_text.replace("'", "")
     my_text=my_text.replace('"', "")
-    my_words = my_text.translate(my_text.maketrans(string.punctuation, " " * len(string.punctuation))).lower().strip().split()
-    # Print the generated array of words (not stemmed yet).
+    my_text=my_text.split()
+    my_words=[]
+    for word in my_text:
+        my_words.append(''.join(c for c in word if (c not in punctuation) or (c == '-')))
+    # Print words before stemming
     print(my_words)
-    # Apply stemming to the list of words.
+    # Apply stemming to the list of words
     my_words = my_stemmer.stem_words(my_words)
-    # Print the generated array of words (already stemmed words).
+    # Print words after stemming
     print(my_words)
